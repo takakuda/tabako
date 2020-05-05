7bdb8a180b93d5ea856880965ca52af189c24841
kei <kei.takakuda.0415@icloud.com>
2020-05-06 04:08:11 +0900

    feat: hello world

--- cmd/root.go
+++ cmd/root.go
@@ -28,7 +28,7 @@ var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   ".",
+	Use:   "hello",
 	Short: "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
@@ -38,7 +38,10 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	RunE: func(cmd *cobra.Command, args []string) error {
+		fmt.Println("hello")
+		return nil
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
